63b5659cb1669aacb7bf1f024df5c6f20e8839b7
WrenHuan9 <wren951017@gmail.com>
2025-05-06 02:21:00 +0200

    Update banner

--- cmdline.py
+++ cmdline.py
@@ -1,6 +1,14 @@
 import optparse
 import sys
 
+KITTY_BANNER = '''
+  _         _                      _                       _     
+ | |__     | |    __ _     __     | |__    __     __ _    | |_   
+ | '_ \    | |   / _` |   / _|    | / /   / _|   / _` |   |  _|  
+ |_.__/   _|_|_  \__,_|   \__|_   |_\_\   \__|_  \__,_|   _\__|  
+_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""|_|"""""| 
+"`-F-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-0-'"`-0-C-' 
+'''
 
 class CommandLines():
 
@@ -10,6 +18,7 @@ class CommandLines():
         parse.add_option('-p', '--proxy', dest='proxy', type=str, help='Please Enter your own Proxy Address')
         parse.add_option('-f', '--flag', dest='ssl_flag', default='0', type=str, help='SSL SEC FLAG')
         (options, args) = parse.parse_args()
+        print(KITTY_BANNER)
         if options.url is None:
             parse.print_help()
             sys.exit(0)
